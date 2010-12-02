c493b00bc0b6f8b359d62acda8ed291e9b9b25a9
Simeon Bird <spb41@cam.ac.uk>
2010-12-02 12:01:39 +0000

    (split)Remove param files and add perl script to generate them instead

---
+++ paramfile.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+( $#ARGV < 0) and die "Please specify a neutrino mass";
+my $o_nu=$ARGV[0]; 
+my $o_cdm=0.25- $o_nu;
+my $paramfile="default-params.ini";
+my $newparams="params-nu$o_nu.ini";
+my @red = ( 4,3,2);
+#Read in template parameter file
+open(my $INHAND, "<","$paramfile") or die "Could not open $paramfile for reading!";
+open(my $OUTHAND, ">","$newparams") or die "Could not open $newparams for writing!";
+while(<$INHAND>){
+        s/^\s*output_root\s*=\s*[\w\/\.-]*/output_root=out\/nu$o_nu/i;
+        #Set neutrino mass
+        s/^\s*use_physical\s*=\s*[\w\/.-]*/use_physical = F/i;
+        s/^\s*omega_cdm\s*=\s*[\w\/.-]*/omega_cdm=$o_cdm/i;
+        s/^\s*omega_neutrino\s*=\s*[\w\/.-]*/omega_neutrino=$o_nu/i;
+        s/^\s*hubble\s*=\s*[\w\/.-]*/hubble = 70/i;
+        #Set things we always need here
+        s/^\s*massless_neutrinos\s*=\s*[\w\/.-]*/massless_neutrinos = 0/i;
+        s/^\s*massive_neutrinos\s*=\s*[\w\/.-]*/massive_neutrinos = 3.04/i;
+        s/^\s*get_transfer\s*=\s*[\w\/.-]*/get_transfer = T/i;
+        s/^\s*do_nonlinear\s*=\s*[\w\/.-]*/do_nonlinear = 1/i;
+        #Set initial conditions; scalar_amp is to give sigma_8 = 0.878 at z=0 with nu=0.
+        #Pivot irrelevant as n_s = 1
+        s/^\s*initial_power_num\s*=\s*[\w\/.-]*/initial_power_num = 1/i;
+        s/^\s*scalar_amp\(1\)\s*=\s*[\w\/.-]*/scalar_amp(1) = 2.27e-9/i;
+        s/^\s*scalar_spectral_index\(1\)\s*=\s*[\w\/.-]*/scalar_spectral_index(1) = 1.0/i;
+        s/^\s*scalar_nrun\(1\)\s*=\s*[\w\/.-]*/scalar_nrun(1) = 0/i;
+        #Set up output
+        s/^\s*transfer_kmax\s*=\s*[\w\/.-]*/transfer_kmax = 10/i;
+        s/^\s*transfer_num_redshifts\s*=\s*[\w\/.-]*/transfer_num_redshifts = $#red/i;
+        s/^\s*transfer_interp_matterpower\s*=\s*[\w\/.-]*/transfer_interp_matterpower = T/i;
+        #Output files set later.
+        for(my $i=0; $i<=$#red; $i++){
+                s/^\s*transfer_redshift\($i\)\s*=\s*[\w\/.-]*//i;
+                s/^\s*transfer_filename\($i\)\s*=\s*[\w\/.-]*//i;
+                s/^\s*transfer_matterpower\($i\)\s*=\s*[\w\/.-]*//i;
+        }
+        #Write to new file.
+        print $OUTHAND $_;
+}
+        #Set output files
+        print $OUTHAND "\n#Transfer output files\n";
+        for(my $i=0; $i<=$#red; $i++){
+                print $OUTHAND "transfer_redshift($i) = $red[$i]\n";
+                print $OUTHAND "transfer_filename($i) = transfer_$red[$i].dat\n";
+                print $OUTHAND "transfer_matterpower($i) = matterpow_$red[$i].dat\n";
+        }
+close($INHAND);
+close($OUTHAND);
+
