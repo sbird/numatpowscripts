5924f03b6da70a33fcf5f714d246e87ad4995783
Simeon Bird <spb41@cam.ac.uk>
2011-01-17 19:44:24 +0000

    (split)Make halofit an argument and add more redshifts

--- paramfile.pl
+++ paramfile.pl
@@ -29,7 +29,7 @@ while(<$INHAND>){
         s/^\s*scalar_spectral_index\(1\)\s*=\s*[\w\/.-]*/scalar_spectral_index(1) = 1.0/i;
         s/^\s*scalar_nrun\(1\)\s*=\s*[\w\/.-]*/scalar_nrun(1) = 0/i;
         #Set up output
-        s/^\s*transfer_kmax\s*=\s*[\w\/.-]*/transfer_kmax = 10/i;
+        s/^\s*transfer_kmax\s*=\s*[\w\/.-]*/transfer_kmax = 30/i;
         my $nout=$#red+1;
         s/^\s*transfer_num_redshifts\s*=\s*[\w\/.-]*/transfer_num_redshifts = $nout/i;
         s/^\s*transfer_interp_matterpower\s*=\s*[\w\/.-]*/transfer_interp_matterpower = T/i;
