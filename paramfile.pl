9b727f19710d7d851933d2bc9927ce9de97bcd1c
Simeon Bird <spb41@cam.ac.uk>
2011-01-18 15:43:00 +0000

    (split)Change paramfile perl script

--- paramfile.pl
+++ paramfile.pl
@@ -1,27 +1,38 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-( $#ARGV < 0) and die "Please specify a neutrino mass";
-my $o_nu=$ARGV[0]; 
+( $#ARGV < 1) and die "Please specify a neutrino mass";
+my $o_nu=shift; 
+my $halo=shift;
 my $o_cdm=0.25- $o_nu;
+my $m_nu=$o_nu*6/13.*100;
 my $paramfile="default-params.ini";
 my $newparams="params-nu$o_nu.ini";
-my @red = ( 4,3,2);
+if(!$halo){
+$newparams="params-nu$o_nu-lin.ini";
+}
+my @red = (9, 4,3,2,1,0.5, 0.2,0);
 #Read in template parameter file
 open(my $INHAND, "<","$paramfile") or die "Could not open $paramfile for reading!";
 open(my $OUTHAND, ">","$newparams") or die "Could not open $newparams for writing!";
 while(<$INHAND>){
-        s/^\s*output_root\s*=\s*[\w\/\.-]*/output_root=out\/nu$o_nu/i;
+        if(!$halo){
+        s/^\s*output_root\s*=\s*[\w\/\.-]*/output_root=out\/nu$m_nu-lin/i;
+        }
+        else{
+        s/^\s*output_root\s*=\s*[\w\/\.-]*/output_root=out\/nu$m_nu/i;
+        }
         #Set neutrino mass
         s/^\s*use_physical\s*=\s*[\w\/.-]*/use_physical = F/i;
         s/^\s*omega_cdm\s*=\s*[\w\/.-]*/omega_cdm=$o_cdm/i;
+        s/^\s*omega_baryon\s*=\s*[\w\/.-]*/omega_baryon=0.05/i;
         s/^\s*omega_neutrino\s*=\s*[\w\/.-]*/omega_neutrino=$o_nu/i;
         s/^\s*hubble\s*=\s*[\w\/.-]*/hubble = 70/i;
         #Set things we always need here
         s/^\s*massless_neutrinos\s*=\s*[\w\/.-]*/massless_neutrinos = 0/i;
         s/^\s*massive_neutrinos\s*=\s*[\w\/.-]*/massive_neutrinos = 3.04/i;
         s/^\s*get_transfer\s*=\s*[\w\/.-]*/get_transfer = T/i;
-        s/^\s*do_nonlinear\s*=\s*[\w\/.-]*/do_nonlinear = 1/i;
+        s/^\s*do_nonlinear\s*=\s*[\w\/.-]*/do_nonlinear = $halo/i;
         #Set initial conditions; scalar_amp is to give sigma_8 = 0.878 at z=0 with nu=0.
         #Pivot irrelevant as n_s = 1
         s/^\s*initial_power_num\s*=\s*[\w\/.-]*/initial_power_num = 1/i;
